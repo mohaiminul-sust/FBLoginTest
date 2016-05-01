cd3f189483424955463a21afa4dc070bdf3f77a9
mohaiminul-sust <mohaiminul.sust@csesociety.org>
2016-05-01 17:38:51 +0600

    added Facebook login

--- logintest/ViewController.swift
+++ logintest/ViewController.swift
@@ -8,18 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate{
 
+    @IBOutlet weak var btnFacebook: FBSDKLoginButton!
+    @IBOutlet weak var ivUserProfile: UIImageView!
+    @IBOutlet weak var lblUserName: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.configureFacebook()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func configureFacebook(){
+        btnFacebook.readPermissions = ["public_profile", "email", "user_friends"]
+        btnFacebook.delegate = self
+    }
+    
+    //login Button Delegates
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        FBSDKGraphRequest.init(graphPath: "me", parameters: ["fields":"first_name, last_name, picture.type(large)"]).startWithCompletionHandler { (connection, result, error) -> Void in
+            
+            let strFirstName: String = (result.objectForKey("first_name") as? String)!
+            let strLastName: String = (result.objectForKey("last_name") as? String)!
+            let strPictureURL: String = (result.objectForKey("picture")?.objectForKey("data")?.objectForKey("url") as? String)!
+            
+            self.lblUserName.text = "Welcome, \(strFirstName) \(strLastName)"
+            self.ivUserProfile.image = UIImage(data: NSData(contentsOfURL: NSURL(string: strPictureURL)!)!)
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        let loginManager: FBSDKLoginManager = FBSDKLoginManager()
+        loginManager.logOut()
+        
+        ivUserProfile.image = nil
+        lblUserName.text = ""
+    }
 
 }
 
